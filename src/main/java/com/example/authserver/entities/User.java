13b54fb012eac85e81f3a264f9e3e39345d8d9d1
Patrick Baciu <patrick.baciu@hotmail.com>
2023-03-01 23:27:29 -0500

    Add user entity and java persistence dependency.

---
+++ src/main/java/com/example/authserver/entities/User.java
@@ -0,0 +1,27 @@
+package com.example.authserver.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String salt;
+    private String streetName;
+    private String streetNumber;
+    private String postalCode;
+    private String city;
+    private String province;
+    private String country;
+}
