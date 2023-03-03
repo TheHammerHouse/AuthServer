2fc9899957ec92822425233731768845a86d47d0
Patrick Baciu <patrick@Patricks-MacBook-Air.local>
2023-03-02 19:37:11 -0500

    Add jwt generation.

--- src/main/java/com/example/authserver/utilities/JWTUtil.java
+++ src/main/java/com/example/authserver/utilities/JWTUtil.java
@@ -1,6 +1,12 @@
 package com.example.authserver.utilities;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class JWTUtil {
@@ -9,7 +15,12 @@ public class JWTUtil {
     }
 
     public String generateAuthToken(String userName) {
-        // TODO implement logic to generate auth token.
-        return "some token";
+        return Jwts.builder()
+                .claim("userName", userName)
+                .setSubject("auth token")
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
+                .compact();
     }
 }
